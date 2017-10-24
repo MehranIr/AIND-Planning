f2a4428e3d8aa58e408e83a16c71215c197378cb
MehranIr <mehran.firoozbakhtan@gmail.com>
2017-10-24 19:48:20 +0200

    finished part one together with the report
    Signed-off-by: MehranIr <mehran.firoozbakhtan@gmail.com>

--- tests/test_my_air_cargo_problems.py
+++ tests/test_my_air_cargo_problems.py
@@ -1,11 +1,14 @@
 import os
 import sys
 parent = os.path.dirname(os.path.realpath(__file__))
+# sys.path.remove(os.path."/tests")
+# parent = "C:\\Users\\mfi\\Documents\\Udacity-AI\\AIND-Planning"
 sys.path.append(os.path.join(os.path.dirname(parent), "aimacode"))
 from aimacode.planning import Action
 from aimacode.utils import expr
 from aimacode.search import Node
 import unittest
+# sys.path.clear()
 from lp_utils import decode_state
 from my_air_cargo_problems import (
     air_cargo_p1, air_cargo_p2, air_cargo_p3,
